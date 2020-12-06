960bcdfd0d6e2705613eaa2f7c716ab0eee05980
Salma-Hashem <63371808+Salma-Hashem@users.noreply.github.com>
2020-12-05 22:29:01 -0500

    Update QuizActivity.java

--- app/src/main/java/com/srm325/budgetshop/QuizActivity.java
+++ app/src/main/java/com/srm325/budgetshop/QuizActivity.java
@@ -1,22 +1,184 @@
 package com.srm325.budgetshop;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.widget.CalendarView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
+    // declare variable for buttons 
+    private Button falseButton;
+    private Button trueButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private ImageView Image;
+    private TextView questionTextView;
+    private int correct = 0;
+    // to keep current question track
+    private int currentQuestionIndex = 0;
 
+    private Question[] questionBank = new Question[] {
+            // array of objects of class Question
+            // providing questions from string
+            // file and holds correct answer
+            new Question(R.string.a, true),
+            new Question(R.string.b, false),
+            new Question(R.string.c, true),
+            new Question(R.string.d, true),
+            new Question(R.string.e, true),
+
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        // setting up the buttons
+        // associated with id
+        falseButton = findViewById(R.id.false_button);
+        trueButton = findViewById(R.id.true_button);
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+        // calling methods for clicking 
+        questionTextView
+                = findViewById(R.id.answer_text_view);
+        Image = findViewById(R.id.myimage);
+        falseButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
     }
 
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View v)
+    {
+        // checking which button is
+        // clicked by user
+        // in this case user choose false
+        switch (v.getId()) {
+            case R.id.false_button:
+                checkAnswer(false);
+                break;
+
+            case R.id.true_button:
+                checkAnswer(true);
+                break;
+
+            case R.id.next_button:
+                // go to next question
+                // number of questions is 5
+                if (currentQuestionIndex < 6) {
+                    currentQuestionIndex
+                            = currentQuestionIndex + 1;
+                    
+                    // last question reached, make buttons invisible
+                    if (currentQuestionIndex == 5) {
+                        questionTextView.setText(getString(
+                                R.string.correct, correct));
+                        nextButton.setVisibility(
+                                View.INVISIBLE);
+                        prevButton.setVisibility(
+                                View.INVISIBLE);
+                        trueButton.setVisibility(
+                                View.INVISIBLE);
+                        falseButton.setVisibility(
+                                View.INVISIBLE);
+                        if (correct > 3) {
+                            Image.setImageResource(
+                                    R.drawable.f6);
+                            questionTextView.setText(
+                                    "SCORE IS " + correct
+                                            + " "
+                                            + "OUT OF 5");
+                            // showing final score with happy image if 3 or more answers correct 
+                        }
+                        else
+                            Image.setImageResource(
+                                    R.drawable.f3);
+                        // if less than 3 correct, display downward trending finances image
+                    }
+                    else {
+                        updateQuestion();
+                    }
+                }
+
+                break;
+            case R.id.prev_button:
+                if (currentQuestionIndex > 0) {
+                    currentQuestionIndex
+                            = (currentQuestionIndex - 1)
+                            % questionBank.length;
+                    updateQuestion();
+                }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void updateQuestion()
+    {
+        Log.d("Current",
+                "onClick: " + currentQuestionIndex);
+
+        questionTextView.setText(
+                questionBank[currentQuestionIndex]
+                        .getAnswerResId());
+        // setting the textview with new question
+        switch (currentQuestionIndex) {
+            case 1:
+                // setting up image for each
+                // question
+                Image.setImageResource(R.drawable.f2);
+                break;
+            case 2:
+                Image.setImageResource(R.drawable.f3);
+                break;
+            case 3:
+                Image.setImageResource(R.drawable.f4);
+                break;
+            case 4:
+                Image.setImageResource(R.drawable.f5);
+                break;
+            case 5:
+                Image.setImageResource(R.drawable.f6);
+                break;
+            case 6:
+                Image.setImageResource(R.drawable.f1);
+                break;
+        }
+    }
+    private void checkAnswer(boolean userChooseCorrect)
+    {
+        boolean answerIsTrue
+                = questionBank[currentQuestionIndex]
+                .isAnswerTrue();
+        // correct answer to question
+        int toastMessageId;
+        // if answer is button which the user clicked then correct 
+
+        if (userChooseCorrect == answerIsTrue) {
+            toastMessageId = R.string.correct_answer;
+            correct++;
+        }
+        else {
+            // showing toast
+            // message correct
+            toastMessageId = R.string.wrong_answer;
+        }
+
+        Toast
+                .makeText(QuizActivity.this, toastMessageId,
+                        Toast.LENGTH_SHORT)
+                .show();
+    }
 }
